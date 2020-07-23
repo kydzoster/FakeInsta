cdf9a1e81e8d35fd32884fe887cb882192c8581f
kydzoster <kydzoster@gmail.com>
2020-07-23 17:55:27 +0100

    added user registration

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,23 @@
-from django import forms 
+from django import forms
+from django.contrib.auth.models import User
 
 # this form will authenticate users against the database
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    # these are the only fields to be included in form, username is a unique field, they cannot be repeated
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        # this will check first password with the second upon registering a new user
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
